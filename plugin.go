74bc6f89f4a75414662416914e9e2698e9f16cf0
injoyai <1113655791@qq.com>
2024-12-26 14:06:12 +0800

    101

--- plugin.go
+++ plugin.go
@@ -1,5 +1,10 @@
 package plugin
 
+import (
+	"fmt"
+	"time"
+)
+
 //type Manage struct {
 //	Code map[string]Interface //代码插件
 //	File map[string]*FileInfo //文件插件
@@ -22,6 +27,34 @@ type Plugin interface {
 	Disable() error //禁用
 	Closed() bool   //是否关闭
 	Err() error     //错误信息
+
+}
+
+type Plugin2 interface {
+	Plugin
+	Log() <-chan Log //日志信息
+	Status() Status  //状态信息
+}
+
+// Log 日志信息
+type Log struct {
+	Time    time.Time `json:"time"`
+	Level   int       `json:"level"`
+	Tag     string    `json:"tag"`
+	Message string    `json:"message"`
+}
+
+func (this Log) String() string {
+	if len(this.Tag) == 0 {
+		return fmt.Sprintf("%s %s", this.Time.Format("2006-01-02 15:04:05"), this.Message)
+	}
+	return fmt.Sprintf("%s [%s] %s", this.Time.Format("2006-01-02 15:04:05"), this.Tag, this.Message)
+}
+
+// Status 状态信息
+type Status struct {
+	Status  int    `json:"status"`  //状态枚举 0:未启用 1:启用
+	Message string `json:"message"` //状态消息
 }
 
 // Info 插件信息
